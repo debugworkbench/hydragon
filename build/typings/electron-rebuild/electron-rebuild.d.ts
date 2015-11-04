6c123ca126eac1e7ba77941e23b6aeb55739c615
Vadim Macagon <vadim.macagon@gmail.com>
2015-11-04 20:01:33 +0700

    Add docs for electron-rebuild type definitions

--- build/typings/electron-rebuild/electron-rebuild.d.ts
+++ build/typings/electron-rebuild/electron-rebuild.d.ts
@@ -3,16 +3,47 @@
 // Definitions by: Vadim Macagon <https://github.com/enlight/>
 
 declare namespace ElectronRebuild {
+  /**
+   * Determine whether native modules need to be rebuilt (not terribly accurate right now).
+   *
+   * @param pathToElectronExecutable Path to the Electron executable native modules should be
+   *                                 rebuilt for.
+   * @param electronVersion If omitted it will be determined from Electron executable.
+   * @return A promise that will be resolved with `true` iff native modules need to be rebuilt.
+   */
   export function shouldRebuildNativeModules(
-    pathToElectronExecutable: string, explicitNodeVersion?: string
+    pathToElectronExecutable: string, electronVersion?: string
   ): Promise<boolean>;
 
+  /**
+   * Download and install the NodeJS SDK for the given Electron runtime.
+   *
+   * @param electronVersion Electron version to download the NodeJS SDK for.
+   * @param nodeDistUrl URL to download the NodeJS SDK from.
+   * @param headersDir The directory to download the NodeJS SDK to.
+   * @param arch `x64` or `ia32`
+   * @return A promise that will be resolved when the operation completes.
+   */
   export function installNodeHeaders(
-    nodeVersion: string, nodeDistUrl?: string, headersDir?: string, arch?: string
+    electronVersion: string, nodeDistUrl?: string, headersDir?: string, arch?: string
   ): Promise<void>;
 
+  /**
+   * Rebuild the native modules in the given `node_modules` directory for the given Electron version.
+   *
+   * @param electronVersion Electron version to rebuild module for.
+   * @param nodeModulesPath The path to the `node_modules` directory containing native modules.
+   * @param whichModule Name of a specific module that should be rebuilt, if specified no other
+   *                    modules will be rebuilt.
+   * @param headersDir Path to the NodeJS SDK to use when rebuilding the native modules.
+   * @return A promise that will be resolved when the operation completes.
+   */
   export function rebuildNativeModules(
-    nodeVersion: string, nodeModulesPath: string, whichModule?: string, headersDir?: string, arch?: string
+    electronVersion: string,
+    nodeModulesPath: string,
+    whichModule?: string,
+    headersDir?: string,
+    arch?: string
   ): Promise<void>;
 }
 
