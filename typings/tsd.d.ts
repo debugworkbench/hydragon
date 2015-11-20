9e32b271fdaaa751ee1975c166b4369875442fc2
Vadim Macagon <vadim.macagon@gmail.com>
2015-11-20 15:00:24 +0700

    Add CodeMirror as a dependency

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="github-electron/github-electron.d.ts" />
 /// <reference path="github-electron/github-electron-main.d.ts" />
 /// <reference path="github-electron/github-electron-renderer.d.ts" />
+/// <reference path="codemirror/codemirror.d.ts" />
