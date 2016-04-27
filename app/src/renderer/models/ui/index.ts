71bb808fde793411f6c52f1fd462fffc0fe40013
Vadim Macagon <vadim.macagon@gmail.com>
2016-04-27 12:27:32 +0700

    Cleanup - sort exports to match directory structure

--- app/src/renderer/models/ui/index.ts
+++ app/src/renderer/models/ui/index.ts
@@ -1,14 +1,17 @@
 // Copyright (c) 2016 Vadim Macagon
 // MIT License, see LICENSE file for full terms.
 
+export { LayoutContainerModel } from './layout/layout-container';
+export { LayoutItemModel } from './layout/layout-item';
 export { PanelModel } from './layout/panel';
 export { SplitterModel } from './layout/splitter';
-export { LayoutItemModel } from './layout/layout-item';
-export { LayoutContainerModel } from './layout/layout-container';
-export { PageModel } from './pages/page';
+
 export { CodeMirrorEditorPageModel } from './pages/code-mirror-editor-page';
 export { GdbMiDebugConfigPageModel } from './pages/gdb-mi-debug-config-page';
+export { PageModel } from './pages/page';
 export { PageSetModel } from './pages/page-set';
 export { PageTreeModel } from './pages/page-tree';
-export { WorkspaceModel } from './workspace';
+
 export { DebugToolbarModel } from './debug-toolbar';
+
+export { WorkspaceModel } from './workspace';
