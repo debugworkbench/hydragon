3a223ad91f3f2f5f4da4bdc3d9b40bde20c8b9e2
Vadim Macagon <vadim.macagon@gmail.com>
2016-05-26 15:54:58 +0700

    Update react-free-style to v2.2.0
    This new release has a "typings" field in its `package.json` so the
    TypeScript compiler can locate the typings by itself. The typings that
    are shipped with the package have to be adjusted to accept an additional
    parameter for the component context, this is done by overwriting the
    `.d.ts` in the package with the customized one stored in `app/typings`.

--- npm-post-install.ts
+++ npm-post-install.ts
@@ -10,3 +10,10 @@ shell.cp(
   path.join(__dirname, 'app/typings/mobx-react/index.d.ts'),
   path.join(__dirname, 'app/node_modules/mobx-react/index.d.ts')
 );
+
+// Replace the default typings that ship with react-free-style with ones that are compatible with
+// the tweaked React typings used in this project.
+shell.cp(
+  path.join(__dirname, 'app/typings/react-free-style/react-free-style.d.ts'),
+  path.join(__dirname, 'app/node_modules/react-free-style/dist/react-free-style.d.ts')
+);
