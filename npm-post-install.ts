4a8f4793b09fd8b0d108c0bbf8a4e41d37ec73ea
Vadim Macagon <vadim.macagon@gmail.com>
2016-06-03 12:58:20 +0700

    Upgrade mobx, mobx-react, mobx-react-devtools packages
    The versions of these packages are now fixed in `package.json` because
    mobx-react and mobx-react-devtools require some post-install tweaking
    of the typings they contain, and that isn't guaranteed to work in any
    later version without some adjustments.

--- npm-post-install.ts
+++ npm-post-install.ts
@@ -4,15 +4,22 @@ import * as path from 'path';
 //shell.cd('app');
 //shell.exec('npm install');
 
-// Replace the default typings that ship with mobx-react with ones that are compatible with the
-// tweaked React typings used in this project.
+// Replace the typings in the mobx-react package with ones that are compatible with the tweaked
+// React typings used in this project.
 shell.cp(
   path.join(__dirname, 'app/typings/mobx-react/index.d.ts'),
   path.join(__dirname, 'app/node_modules/mobx-react/index.d.ts')
 );
 
-// Replace the default typings that ship with react-free-style with ones that are compatible with
-// the tweaked React typings used in this project.
+// Replace the typings in the mobx-react-devtools package with ones that are compatible with the
+// tweaked React typings used in this project.
+shell.cp(
+  path.join(__dirname, 'app/typings/mobx-react-devtools/index.d.ts'),
+  path.join(__dirname, 'app/node_modules/mobx-react-devtools/index.d.ts')
+);
+
+// Replace the typings in the react-free-style package with ones that are compatible with the
+// tweaked React typings used in this project.
 shell.cp(
   path.join(__dirname, 'app/typings/react-free-style/react-free-style.d.ts'),
   path.join(__dirname, 'app/node_modules/react-free-style/dist/react-free-style.d.ts')
