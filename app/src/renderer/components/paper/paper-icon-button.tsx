18658caefdb8bb45fd8fc329c5a6e15ab3798b49
Vadim Macagon <vadim.macagon@gmail.com>
2016-06-09 15:20:49 +0700

    Make it easier to style PaperIconButtonComponent with CSS mixins

--- app/src/renderer/components/paper/paper-icon-button.tsx
+++ app/src/renderer/components/paper/paper-icon-button.tsx
@@ -3,6 +3,7 @@
 
 import * as React from 'react';
 import { PolymerComponent } from './polymer';
+import { omitOwnProps } from '../../../common/utils';
 
 /**
  * React component that wraps a Polymer paper-icon-button custom element.
@@ -11,6 +12,24 @@ export class PaperIconButtonComponent
        extends PolymerComponent<
                  PolymerElements.PaperIconButton, PaperIconButtonComponent.IProps, {}> {
 
+  protected get cssVars() {
+    const styles = this.props.styles;
+    const vars: any = {};
+
+    if (styles) {
+      if (styles.enabled) {
+        vars['--paper-icon-button'] = styles.enabled;
+      }
+      if (styles.disabled) {
+        vars['--paper-icon-button-disabled'] = styles.disabled;
+      }
+      if (styles.hover) {
+        vars['--paper-input-button-hover'] = styles.hover;
+      }
+    }
+    return vars;
+  }
+
   protected get eventBindings() {
     return [
       { event: 'tap', listener: 'onDidTap' }
@@ -18,8 +37,9 @@ export class PaperIconButtonComponent
   }
 
   protected renderElement(props: PaperIconButtonComponent.IProps) {
+    const elementProps = omitOwnProps(props, ['styles']);
     return (
-      <paper-icon-button {...props}></paper-icon-button>
+      <paper-icon-button {...elementProps}></paper-icon-button>
     );
   }
 }
@@ -27,6 +47,14 @@ export class PaperIconButtonComponent
 namespace PaperIconButtonComponent {
   export interface IProps extends PolymerComponent.IProps {
     icon?: string;
+    styles?: {
+      /** CSS mixin to apply to the paper-icon-button. */
+      enabled?: React.CSSProperties;
+      /** CSS mixin to apply to the paper-icon-button when it's disabled. */
+      disabled?: React.CSSProperties;
+      /** CSS mixin to apply to the paper-icon-button on hover. */
+      hover?: React.CSSProperties;
+    };
     /** Callback to invoke after the user taps on the button. */
     onDidTap?: (e: polymer.TapEvent) => void;
   }
